4db32f3b2d43969c7f3c361599360944ba757fd7
chinhdinh11200 <chinhdinhtho@gmail.com>
2021-12-20 13:31:40 +0700

    Bai tap ma hoa AES

---
+++ src/aes_at/bm/AES_ATBM.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aes_at.bm;
+
+import aes.AES_Test;
+import java.io.File;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class AES_ATBM {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String key;
+
+        key = "1qaz2wsx3edc4rfv5tgb6yhn";
+    }
+    
+}
